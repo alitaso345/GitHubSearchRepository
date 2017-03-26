eede28bd4c3e13b41becd55bd8b3249b56c76191
arisu-maruyama <arisu-maruyama@cookpad.com>
2017-03-27 00:45:02 +0900

    Initial Commit

---
+++ GitHubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubSearchRepository
+//
+//  Created by arisu-maruyama on 2017/03/27.
+//  Copyright © 2017年 alitaso345. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
