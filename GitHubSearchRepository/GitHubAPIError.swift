58d5a793d05c656e727590f2446f20a7173b95f8
arisu-maruyama <arisu-maruyama@cookpad.com>
2017-03-27 22:19:41 +0900

    Add GitHubAPIError struct

--- GitHubSearchRepository/GitHubAPIError.swift
+++ GitHubSearchRepository/GitHubAPIError.swift
@@ -1,9 +1,22 @@
-//
-//  GitHubAPIError.swift
-//  GitHubSearchRepository
-//
-//  Created by arisu-maruyama on 2017/03/27.
-//  Copyright © 2017年 alitaso345. All rights reserved.
-//
+//JSON
+//{
+//  "message": "Not Found"
+//}
 
 import Foundation
+
+struct GitHubAPIError : JSONDecodable, Error {
+    let message: String
+
+    init(json: Any) throws {
+        guard let dictionary = json as? [String : Any] else {
+            throw JSONDecodeError.invalidFormat(json: json)
+        }
+
+        guard let message = dictionary["message"] as? String else {
+            throw JSONDecodeError.missingValue(key: "message", actualValue: dictionary["message"])
+        }
+
+        self.message = message
+    }
+}
