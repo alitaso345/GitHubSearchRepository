9b66ca79bd6ad95685ce6a82c21c0980e9e6fe01
arisu-maruyama <arisu-maruyama@cookpad.com>
2017-03-27 10:23:40 +0900

    Add User swift

---
+++ GitHubSearchRepository/Repository.swift
@@ -0,0 +1,9 @@
+//
+//  Repository.swift
+//  GitHubSearchRepository
+//
+//  Created by arisu-maruyama on 2017/03/27.
+//  Copyright © 2017年 alitaso345. All rights reserved.
+//
+
+import Foundation
---
+++ GitHubSearchRepository/User.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct User {
+    let id: Int
+    let login: String
+}
