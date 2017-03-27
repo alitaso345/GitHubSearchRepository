0b335cbb8a044be4b3f72b83bdceb9c18edd3bce
arisu-maruyama <arisu-maruyama@cookpad.com>
2017-03-28 02:27:32 +0900

    Add main script

--- GitHubSearchRepository/main.swift
+++ GitHubSearchRepository/main.swift
@@ -1,12 +1,39 @@
-//
-//  main.swift
-//  GitHubSearchRepository
-//
-//  Created by arisu-maruyama on 2017/03/27.
-//  Copyright © 2017年 alitaso345. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+print("Enter your query here> ", terminator: "")
 
+//入力された検索クエリの取得
+guard let keyword = readLine(strippingNewline: true) else {
+    exit(1)
+}
+
+//APIクライアントの生成
+let client = GitHubClient()
+
+//リクエストの発行
+let request = GitHubAPI.SearchRepositories(keyword: keyword)
+
+//リクエストの送信
+client.send(request: request) { result in
+    switch result {
+    case let .success(response):
+        for item in response.items {
+            print(item.owner.login + "/" + item.name)
+        }
+        exit(0)
+    case let .failure(error):
+        //エラー詳細を出力
+        print(error)
+        exit(1)
+    }
+}
+
+//タイムアウト時間
+let timeoutInterval: TimeInterval = 60
+
+//タイムアウトまでメインスレッドを停止
+Thread.sleep(forTimeInterval: timeoutInterval)
+
+//タイムアウト後の処理
+print("Connection timeout")
+exit(1)
